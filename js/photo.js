4da084faa0d1a01ed5b48cf0ae6ec490ff3a9119
Andy Zhang <wenyuzhang@msn.cn>
2017-05-01 00:33:37 -0400

    update
    semi_final

---
+++ js/photo.js
@@ -0,0 +1,13 @@
+function getRandomSize(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+var allImages = "";
+
+for (var i = 0; i < 25; i++) {
+  var width = getRandomSize(200, 400);
+  var height =  getRandomSize(200, 400);
+  allImages += '<img src="https://placekitten.com/'+width+'/'+height+'" alt="pretty kitty">';
+}
+
+$('#photos').append(allImages);
